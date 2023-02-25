80987acd785753c932667fecf5a742cd7815b6f2
camelStyleUser <124447863+camelStyleUser@users.noreply.github.com>
2023-02-25 14:15:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
-import sys
+import sys,math
 import pygame
 from pygame.locals import *
 pygame.init()
 surface = pygame.display.set_mode((640,640))
 player=pygame.image.load("player.png")
 rot=0
-speed=1
+speed=4
 playerrect=player.get_rect()
 clock=pygame.time.Clock()
-playerrect.move((298,298))
+x=0
+y=0
+angle=0;
 while True:
   for ev in pygame.event.get():
     if ev.type== QUIT:
       pygame.quit()
   clock.tick(60)
-  surface.fill((0,0,0))
-  surface.blit(player,playerrect)
-  pygame.display.flip()
-  keys=pygame.key.get_pressed()
   keys = pygame.key.get_pressed()
-  playerrect.move([(keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * speed,0])
-  playerrect.move([0,(keys[pygame.K_DOWN] - keys[pygame.K_UP]) * speed])
+  x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * speed
+  y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * speed
+  playerrect.x+=x
+  playerrect.y+=y
+  playerrect.centerx = playerrect.centerx % surface.get_width()
+  playerrect.centery = playerrect.centery % surface.get_height()
+  try:
+    angle=(180+math.degrees(math.atan2(x,y)))
+  except:
+    pass
+  surface.fill((0,0,0))
+  rplayer=pygame.transform.rotate(player,angle)
+  rrect=rplayer.get_rect(center=playerrect.center)
+  surface.blit(rplayer,rrect)
+  x=0
+  y=0
+  pygame.display.flip()
+
   
+  
+
   
