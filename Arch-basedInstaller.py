162752b80a1a3dadb6fd08bf0cc9fc67af4e54df
CurrentlyIsGlitching <126282768+CurrentlyIsGlitching@users.noreply.github.com>
2023-06-28 16:26:40 +0500

    Create Arch-basedInstaller.py

---
+++ Arch-basedInstaller.py
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int main()
+
+{
+
+  char x;
+
+  printf("Установщик gamepygame для arch-based linux\nпродолжить? [Y/N]");
+
+  scanf("%c", &x);
+
+  if(x == 'Y'){
+
+    system("cd $HOME && pacman -S python3 python-pip wget && pip install pygame && mkdir .gamepygame && echo \"python $HOME/.gamepygame/main.py\" >> && chmod +x $PREFIX/bin/gamepygame")
+    printf("Введите gamepygame чтобы запустить игру.")
+
+  }
+
+}
