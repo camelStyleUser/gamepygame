8561964e8b58bd6170e35f943d56a89e379f79ed
NecessaryAgama <126282768+NecessaryAgama@users.noreply.github.com>
2023-08-27 03:58:29 +0500

    Update Arch-basedInstaller.c
    fixed my mistake

--- Arch-basedInstaller.c
+++ Arch-basedInstaller.c
@@ -6,7 +6,7 @@ int main()
   printf("Установщик gamepygame для arch-based linux\nпродолжить? [Y/N]");
   scanf("%c", &x);
   if(x == 'Y'){
-    system("cd $HOME && sudo pacman -S python3 python-pip wget --noconfirm && pip install pygame --break-system-packages && mkdir .gamepygame && echo \"python $HOME/.gamepygame/main.py\" >> $PREFIX/bin/gamepygame && chmod +x $PREFIX/bin/gamepygame);
+    system("cd $HOME && sudo pacman -S python3 python-pip wget --noconfirm && pip install pygame --break-system-packages && mkdir .gamepygame && echo \"python $HOME/.gamepygame/main.py\" >> $PREFIX/bin/gamepygame && chmod +x $PREFIX/bin/gamepygame");
     printf("Введите gamepygame чтобы запустить игру.");
   }
 }
