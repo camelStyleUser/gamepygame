6e4fb40e5f288ff6b3c2528954319550898ff5e6
NecessaryAgama <126282768+NecessaryAgama@users.noreply.github.com>
2023-08-27 03:58:00 +0500

    Update Arch-basedInstaller.c
    added --noconfirm flag to package manager and made pip work

--- Arch-basedInstaller.c
+++ Arch-basedInstaller.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 int main()
-
 {
-
   char x;
-
   printf("Установщик gamepygame для arch-based linux\nпродолжить? [Y/N]");
-
   scanf("%c", &x);
-
   if(x == 'Y'){
-
-    system("cd $HOME && sudo pacman -S python3 python-pip wget && pip install pygame && mkdir .gamepygame && echo \"python $HOME/.gamepygame/main.py\" >> && chmod +x $PREFIX/bin/gamepygame");
+    system("cd $HOME && sudo pacman -S python3 python-pip wget --noconfirm && pip install pygame --break-system-packages && mkdir .gamepygame && echo \"python $HOME/.gamepygame/main.py\" >> $PREFIX/bin/gamepygame && chmod +x $PREFIX/bin/gamepygame);
     printf("Введите gamepygame чтобы запустить игру.");
-
   }
-
 }
